36c0be862c5c2aaea3d6711fb6aa136f8048803e
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-02-19 11:20:30 +0300

    added argparse support to vector gen script

--- test_vector_gen.py
+++ test_vector_gen.py
@@ -1,9 +1,15 @@
 import re
+import os
 import sys
 import json
+import argparse
 
 DEBUG = False
 SAVE_FILE = "test_vectors.txt"
+
+COLOR_RED = "\u001b[31;1m"
+COLOR_BLUE = "\u001b[34;1m"
+COLOR_RESET = "\u001b[0m"
 
 binarize = lambda n: 0 if n <= 0 else 1
 
@@ -11,8 +17,11 @@ def debug(*args, **kwargs):
     if DEBUG:
         print("[>>] ", *args, **kwargs)
 
-def show_usage():
-    print(f"[i] Usage: {sys.argv[0]} <config-file>")
+def error(*args, **kwargs):
+    print(COLOR_RED, "[!!!] Error:", COLOR_RESET, *args, **kwargs)
+
+def info(*args, **kwargs):
+    print(COLOR_BLUE, "[[i]]", COLOR_RESET, *args, **kwargs)
 
 class Bit(object):
     def __init__(self, value=0, call_code=None):
@@ -45,12 +54,28 @@ class LazyVariable(object):
     def __call__(self):
         return eval(self.__call_code)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        show_usage()
-        sys.exit()
+testgen_parser = argparse.ArgumentParser(
+    description="Logisim test vector generator script for Sazak's CENG232 TestBench",
+    epilog="Made with ❤️ by Ozan Sazak.\nHappy coding :)")
 
-    with open(sys.argv[1], "r") as fp:
+testgen_parser.add_argument("config_file", type=str,
+    help="Path of test configuration file")
+testgen_parser.add_argument("-o", "--output", default=SAVE_FILE,
+    type=str, help="Save the test vector to a custom path")
+testgen_parser.add_argument("-v", "--verbose", action="store_true",
+    help="Show debug messages (Increase verbosity)")
+
+if __name__ == "__main__":
+    args = testgen_parser.parse_args()
+
+    if args.verbose:
+        DEBUG = True
+
+    if not os.access(args.config_file, os.R_OK):
+        error(f"Config file '{args.config_file}' is not reachable.")
+        sys.exit(1)
+
+    with open(args.config_file, "r") as fp:
         test_config = json.load(fp)
 
     symtab = dict()
@@ -109,7 +134,7 @@ if __name__ == "__main__":
 
         test_vector_lines.append(" ".join(line) + "\n")
 
-    debug("Saving test vectors to:", SAVE_FILE)
+    debug("Saving test vectors to:", args.output)
 
-    with open(SAVE_FILE, "w") as fp:
+    with open(args.output, "w") as fp:
         fp.writelines(test_vector_lines)
